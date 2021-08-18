afb5028131f139ead40e1f47261cf1249626d614
Hiep Trinh <hieptq.social@gmail.com>
2021-08-18 21:52:46 +1000

    change: testing husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
 export default function App() {
-  return <h1>React from scratch with Clark</h1>;
+  return <h1>React from scratch with Clark. This is another test</h1>;
 }
